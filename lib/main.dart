64da6369012b88e387ecff2d79b49a71855bc834
Majmos <mimsowy@gmail.com>
2022-11-20 18:03:04 +0100

    implemented home page view and about dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'my_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,121 +26,20 @@ class MyApp extends StatelessWidget {
         primarySwatch: const MaterialColor(
           0xFF222222,
           <int, Color>{
-            50: Color(0xFF000000),
-            100: Color(0xFF000000),
-            200: Color(0xFF000000),
-            300: Color(0xFF000000),
-            400: Color(0xFF000000),
+            50: Color(0xFFAAAAAA),
+            100: Color(0xFF888888),
+            200: Color(0xFF666666),
+            300: Color(0xFF555555),
+            400: Color(0xFF444444),
             500: Color(0xFF222222),
-            600: Color(0xFF000000),
-            700: Color(0xFF000000),
-            800: Color(0xFF000000),
+            600: Color(0xFF202020),
+            700: Color(0xFF1A1A1A),
+            800: Color(0xFF101010),
             900: Color(0xFF000000),
           },
         ),
       ),
-      home: const MyHomePage(title: 'Drugsistant Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      backgroundColor: Colors.black,
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        backgroundColor: const Color(0xFF222222),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
-            label: "HOME",
-            backgroundColor: Color(0xFF222222),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.medication_outlined),
-            activeIcon: Icon(Icons.medication),
-            label: "DRUGS",
-            backgroundColor: Color(0xFF222222),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month_outlined),
-            activeIcon: Icon(Icons.calendar_month),
-            label: "CALLENDAR",
-            backgroundColor: Color(0xFF222222),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info_outlined),
-            activeIcon: Icon(Icons.info),
-            label: "ABOUT",
-            backgroundColor: Color(0xFF222222),
-          ),
-        ],
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[],
-        ),
-      ),
+      home: const MyPages(),
     );
   }
 }
---
+++ lib/my_pages.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:pim/home_page.dart';
+
+class MyPages extends StatefulWidget {
+  const MyPages({super.key});
+
+  @override
+  State<MyPages> createState() => _MyPagesState();
+}
+
+class _MyPagesState extends State<MyPages> {
+  int _selectedIndex = 0;
+
+// TODO Implement other pages
+  final _widgetOptions = const <Widget>[
+    HomePage(),
+    Text(
+      "Drugs",
+      style: TextStyle(color: Colors.white),
+    ),
+    Text(
+      "Calendar",
+      style: TextStyle(color: Colors.white),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_widgetOptions.elementAt(_selectedIndex).toStringShort()),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () => {/* TODO implement logic */},
+                  icon: const Icon(Icons.notifications)),
+              IconButton(
+                  onPressed: () => {showAboutDialog(context: context)},
+                  icon: const Icon(Icons.info)),
+            ],
+          )
+        ],
+      ),
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: const Color(0xFF222222),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: "HOME",
+            backgroundColor: Color(0xFF222222),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.medication_outlined),
+            activeIcon: Icon(Icons.medication),
+            label: "DRUGS",
+            backgroundColor: Color(0xFF222222),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined),
+            activeIcon: Icon(Icons.calendar_month),
+            label: "CALENDAR",
+            backgroundColor: Color(0xFF222222),
+          ),
+        ],
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      drawer: Drawer(
+        backgroundColor: const Color(0xFF222222),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Header"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

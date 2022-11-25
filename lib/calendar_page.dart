56dac6cecab8417a7fa3e6b83802549de590468b
Majmos <mimsowy@gmail.com>
2022-11-26 00:49:20 +0100

    modified calendar

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -1,21 +1,52 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class CalendarPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
 
-  void onDateChanged(DateTime time) {
-    // TODO Load scrollable list of drugs to take that day and show them below calendar
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime selectedDay = DateTime.now();
+// TODO Load scrollable list of drugs to take that day and show them below calendar
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      selectedDay = day;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CalendarDatePicker(
-          initialDate: DateTime.now(),
-          firstDate: DateTime.utc(1990),
-          lastDate: DateTime.utc(2030),
-          onDateChanged: onDateChanged,
+        TableCalendar(
+          focusedDay: selectedDay,
+          firstDay: DateTime.utc(1990),
+          lastDay: DateTime.utc(2030),
+          calendarFormat: CalendarFormat.month,
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+          ),
+          calendarStyle: CalendarStyle(
+            selectedDecoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              shape: BoxShape.circle,
+            ),
+            todayDecoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary,
+                width: 2,
+              ),
+              shape: BoxShape.circle,
+            ),
+          ),
+          onDaySelected: _onDaySelected,
+          selectedDayPredicate: (day) => isSameDay(day, selectedDay),
         ),
         const Tab(
           text: "Placeholder for scrollable list",

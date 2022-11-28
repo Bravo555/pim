45053b39e6402d768f6e653ada94b0c98003a67b
Majmos <mimsowy@gmail.com>
2022-11-28 22:58:09 +0100

    Created drug model

--- lib/drug_list_item.dart
+++ lib/drug_list_item.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:pim/drug_details_page.dart';
+import 'package:pim/models/drug.dart';
 
 class DrugListItem extends StatelessWidget {
   const DrugListItem({
     super.key,
-    required this.drugName,
+    required this.drug,
     required this.nextDoseDate,
-    required this.id,
   });
 
-  final String drugName;
+  final Drug drug;
   final DateTime nextDoseDate;
-  final int id;
 
   _onSelected(BuildContext context, String value) {
     if (value == 'edit') {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => DrugDetailsPage(id: id),
+          builder: (context) => DrugDetailsPage(id: drug.id),
         ),
       );
     } else {
@@ -31,7 +30,7 @@ class DrugListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       enabled: true,
-      title: Text(drugName),
+      title: Text(drug.name),
       subtitle: Text("Next dose ${nextDoseDate.toString().substring(0, 16)}"),
       trailing: PopupMenuButton(
         onSelected: (value) => _onSelected(context, value),
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pim/models/drug.dart';
 import 'decorated_container.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,12 +15,47 @@ class HomePage extends StatelessWidget {
         children: <Widget>[
           DecoratedContainer(
             isColoured: true,
-            child: const Text(
-              "Placeholder",
-            ),
+            drugs: <Drug>[
+              Drug(
+                id: 1,
+                name: "Espumisan",
+                dosage: "20mg",
+                shouldNotify: true,
+              ),
+              Drug(
+                id: 2,
+                name: "Aspiryna",
+                dosage: "10mg",
+                shouldNotify: true,
+              )
+            ],
           ),
-          DecoratedContainer(),
-          DecoratedContainer(),
+          DecoratedContainer(
+            drugs: <Drug>[
+              Drug(
+                id: 3,
+                name: "Bianacid",
+                dosage: "40mg",
+                shouldNotify: true,
+              ),
+            ],
+          ),
+          DecoratedContainer(
+            drugs: <Drug>[
+              Drug(
+                id: 4,
+                name: "No-Spa",
+                dosage: "10mg",
+                shouldNotify: false,
+              ),
+              Drug(
+                id: 1,
+                name: "Espumisan",
+                dosage: "20mg",
+                shouldNotify: true,
+              ),
+            ],
+          ),
         ],
       ),
     );
